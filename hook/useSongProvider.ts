4c96126d9ffd550a29c09e6d9eab12beb895cca1
Sagar Manchakatla <sagarmanchakatla01@gmail.com>
2025-03-05 19:33:38 +0530

    mood based song recomdation done

--- hook/useSongProvider.ts
+++ hook/useSongProvider.ts
@@ -3,81 +3,86 @@ import { search } from "@/http/songs";
 import { router } from "expo-router";
 import React, { useContext } from "react";
 
-
-
 export default function useSongProvider() {
-    const context = useContext(SearchProviderContext);
-    const { data: { artists, playlists, songs }, error, loading, currentlyPlaying, isPlaying, updateState } = context
+  const context = useContext(SearchProviderContext);
+  const {
+    data: { artists, playlists, songs },
+    error,
+    loading,
+    currentlyPlaying,
+    isPlaying,
+    updateState,
+  } = context;
 
-    const fetchSearchQuery = async (query: string) => {
-        try {
-            updateState(pre => ({
-                ...pre,
-                loading: true
-            }))
-            const response = await search(query);
-            updateState(pre => ({
-                ...pre,
-                data: {
-                    artists: response.artists,
-                    playlists: response.playlists,
-                    songs: response.songs
-                },
-                currentlyPlaying: 0,
-                loading: false
-            }))
-        } catch (err: any) {
-            console.log(err.message);
-            updateState(pre => ({
-                ...pre,
-                loading: false,
-                error: err.message
-            }));
-        }
+  const fetchSearchQuery = async (query: string) => {
+    try {
+      updateState((pre) => ({
+        ...pre,
+        loading: true,
+      }));
+      const response = await search(query);
+      updateState((pre) => ({
+        ...pre,
+        data: {
+          artists: response.artists,
+          playlists: response.playlists,
+          songs: response.songs,
+        },
+        currentlyPlaying: 0,
+        loading: false,
+      }));
+    } catch (err: any) {
+      console.log(err.message);
+      updateState((pre) => ({
+        ...pre,
+        loading: false,
+        error: err.message,
+      }));
     }
-    const setSongPlay = (id: number) => {
-        updateState(pre => ({
-            ...pre,
-            currentlyPlaying: id,
-            isPlaying: true,
-        }))
-        router.push("/(root)/musicPlayer");
+  };
+  const setSongPlay = (id: number) => {
+    updateState((pre) => ({
+      ...pre,
+      currentlyPlaying: id,
+      isPlaying: true,
+    }));
+    router.push("/(root)/musicPlayer");
+  };
+  const getCurrentSongDetailes = () => {
+    if (currentlyPlaying! >= 0 && currentlyPlaying! < songs.length) {
+      return songs[currentlyPlaying!];
     }
-    const getCurrentSongDetailes = () => {
-        if (currentlyPlaying! >= 0 && currentlyPlaying! < songs.length) {
-            return songs[currentlyPlaying!]
-        }
-        return null;
-    }
+    return null;
+  };
 
-    const handleNext = () => {
-        if (currentlyPlaying! < songs.length) {
-            setSongPlay(currentlyPlaying! + 1)
-        }
+  const handleNext = () => {
+    if (currentlyPlaying! < songs.length) {
+      setSongPlay(currentlyPlaying! + 1);
     }
-    const handlePrevious = () => {
-        if (currentlyPlaying! > 0) {
-            setSongPlay(currentlyPlaying! - 1);
-        }
+  };
+  const handlePrevious = () => {
+    if (currentlyPlaying! > 0) {
+      setSongPlay(currentlyPlaying! - 1);
     }
-    const handleShuffle = () => {
-        const shuffleSongId = Math.floor(Math.random() * songs.length - 1);
-        setSongPlay(shuffleSongId);
-    }
+  };
+  const handleShuffle = () => {
+    const shuffleSongId = Math.floor(Math.random() * songs.length - 1);
+    setSongPlay(shuffleSongId);
+  };
 
-    return {
-        songs,
-        artists,
-        playlists,
-        loading,
-        error,
-        isPlaying,
-        currentlyPlaying,
-        handleShuffle,
-        handleNext,
-        handlePrevious,
-        setSongPlay,
-        getCurrentSongDetailes,
-        fetchSearchQuery
-    }
+  return {
+    songs,
+    artists,
+    playlists,
+    loading,
+    error,
+    isPlaying,
+    currentlyPlaying,
+    handleShuffle,
+    handleNext,
+    handlePrevious,
+    setSongPlay,
+    getCurrentSongDetailes,
+    fetchSearchQuery,
+  };
 }
--- http/index.ts
+++ http/index.ts
@@ -2,28 +2,30 @@ import { FaceEmotionSchema } from "@/schema/emotion";
 
 const BASEURL = "https://face-api2.p.rapidapi.com/api/FaceOrchestrator";
 
-
-export const detectEmotion = async (base64encodedimage:string):Promise<FaceEmotionSchema>=>{
-    const response = await fetch(`${BASEURL}`,{
-        method : "POST",
-        body : JSON.stringify({
-            source: base64encodedimage,
-            sourceType: 'base64',
-            detectGender: false,
-            detectEmotion: true,
-            detectEthnicity: false,
-            detectLandmarks: false,
-            detectAge: false
-        }),
-        headers : {
-            'x-rapidapi-key' : '5d1697b208mshd28835166cf7e93p10a73fjsn9c8d425399f5',
-            'x-rapidapi-host' : 'face-api2.p.rapidapi.com',
-            'Content-Type' : 'application/json'   
-        }
-    });
-    if(!response.ok){
-        console.log(await response.json())
-        throw Error("Can't detect the Emotion for now try again later");
-    }
-    return response.json();
-}
+export const detectEmotion = async (
+  base64encodedimage: string
+): Promise<FaceEmotionSchema> => {
+  const response = await fetch(`${BASEURL}`, {
+    method: "POST",
+    body: JSON.stringify({
+      source: base64encodedimage,
+      sourceType: "base64",
+      detectGender: false,
+      detectEmotion: true,
+      detectEthnicity: false,
+      detectLandmarks: false,
+      detectAge: false,
+    }),
+    headers: {
+      "x-rapidapi-key": "5d1697b208mshd28835166cf7e93p10a73fjsn9c8d425399f5",
+      "x-rapidapi-host": "face-api2.p.rapidapi.com",
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    console.log(await response.json());
+    throw Error("Can't detect the Emotion for now try again later");
+  }
+  //   console.log(response.json());
+  return response.json();
+};
